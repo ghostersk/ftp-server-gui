9110a921293312d3bb2ca51416da12ddc1e4bd12
hdvpdrm <hardcoderyan@gmail.com>
2025-09-01 07:35:09 +0000

    run tui mode

--- __main__.py
+++ __main__.py
@@ -4,5 +4,9 @@ if __name__ == '__main__':
     arguments = get_arguments()
     if "TUI" == arguments.mode:
         print("starting FTP server in TUI mode.")
+        
+        from tui.tui import TUI
+        app = TUI()
+        app.run()
     if "GUI" == arguments.mode:
         print("starting FTP server in GUI mode.")
